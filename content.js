6b9af9143ae3183cee9126384ab5a06b5450757c
l08084 <l08084.1989@gmail.com>
2020-12-28 15:19:28 +0900

    add todo comment

--- content.js
+++ content.js
@@ -1,7 +1,19 @@
 window.onload = function() {
     const birthdayElements = document.getElementsByClassName('bday');
-    if (birthdayElements.length > 0) {
-        const birthdayText = birthdayElements[0].textContent;
-        window.alert(birthdayText);
+    if (birthdayElements.length === 0) {
+        return;
     }
+    const birthdayText = birthdayElements[0].textContent;
+    if (!birthdayText) {
+        return;
+    }
+    const birthdayYearNumber
+        = Number(new Date(birthdayText.slice(0, 4), '01', '01').getFullYear());
+    window.alert(document.body.innerText);
+    const bodyText = document.body.innerText;
+    // TODO: YYYY年の文字列を全てYYYY年(当時XX歳)に置換する
+    // TODO: YYYY年のインデックスを取得(search?)し、インデックスを元にsliceで取得、
+    // TODO: 取得した文字列を元に、当時の年齢を計算する。
+    // TODO: YYYY年(当時XX歳)をjoinで挿入する
+    const spliteTextList = bodyText.split(/\s*(?:;|$)\s*/);
 };
