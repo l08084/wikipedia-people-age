66987daa9b2376e1b652f6dd5d44a6c971d03e72
l08084 <l08084.1989@gmail.com>
2020-12-28 19:09:04 +0900

    難しい

--- content.js
+++ content.js
@@ -9,11 +9,18 @@ window.onload = function() {
     }
     const birthdayYearNumber
         = Number(new Date(birthdayText.slice(0, 4), '01', '01').getFullYear());
-    window.alert(document.body.innerText);
     const bodyText = document.body.innerText;
     // TODO: YYYY年の文字列を全てYYYY年(当時XX歳)に置換する
     // TODO: YYYY年のインデックスを取得(search?)し、インデックスを元にsliceで取得、
     // TODO: 取得した文字列を元に、当時の年齢を計算する。
     // TODO: YYYY年(当時XX歳)をjoinで挿入する
-    const spliteTextList = bodyText.split(/\s*(?:;|$)\s*/);
+    // TODO: どうやって文章を更新するのか.........
+    while (bodyText.search(/\d{4}年/g) !== -1) {
+        const yearText = bodyText.slice(search(/\d{4}年/g), search(/\d{4}年/g) + 4);
+        const age = Number(yearText) - birthdayYearNumber;
+        if (age > 0) {
+            const ageText = `(当時${age}歳)`;
+        }
+    }
+    document.body.innerText = bodyText;
 };
